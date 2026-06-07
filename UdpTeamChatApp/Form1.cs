a5e6df23942d0130bb5a1c5ba4d92e2321addcd0
BebrOchiK <oleksiychukzakhar@gmail.com>
2026-06-07 10:41:20 +0300

    Base version created (Client-Server variant)

--- UdpTeamChatApp/Form1.cs
+++ UdpTeamChatApp/Form1.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace UdpTeamChatApp
 {
     public partial class Form1 : Form
@@ -5,6 +9,32 @@ namespace UdpTeamChatApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = textBox3.Text;
+            if (string.IsNullOrEmpty(message) )
+            {
+                MessageBox.Show("Enter message first");
+                return;
+            }
+
+            using (UdpClient udpClient = new UdpClient())
+            {
+                try
+                {
+                    byte[] buff = Encoding.UTF8.GetBytes(message);
+                    IPAddress address = IPAddress.Parse(textBox1.Text);
+                    int port = int.Parse(textBox2.Text);
+                    udpClient.Send(buff, buff.Length, new IPEndPoint(address, port));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
         }
     }
 }
---
+++ UdpTeamChatAppClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+int port = 10000;
+UdpClient udpServer = new UdpClient(port);
+
+try
+{
+    IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+    while (true)
+    {
+        byte[] buff = udpServer.Receive(ref remoteEP);
+        string receivedText = Encoding.UTF8.GetString(buff);
+        Console.WriteLine(receivedText);
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally { udpServer?.Close(); }
