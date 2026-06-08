96ad566963839968991ddf15a3f2a7f74118d193
Misha <mikhailptu@gmail.com>
2026-06-08 14:06:48 +0200

    Organized classes

---
+++ Models/Chat.cs
@@ -0,0 +1,10 @@
+namespace Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<User> Members {get; set;}
+        public bool IsGroup { get; set; }
+    } 
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string IPAddress { get; set; }
+        public int Port { get; set; }
+        public UserStatus Status { get; set; }
+        public DateTime OfflineFromTime { get; set; }
+
+    }
+
+    public enum UserStatus
+    {
+        Offline,
+        Online
+    }
+}
