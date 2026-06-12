c20997e5a971e348bea4bcf3dca85cbf67d33a28
1odc <vlgamerm12349@gmail.com>
2026-06-12 21:25:18 +0200

    Destroyed

--- UdpTeamChatApp/Form1.cs
+++ UdpTeamChatApp/Form1.cs
@@ -11,30 +11,6 @@ namespace UdpTeamChatApp
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string message = textBox3.Text;
-            if (string.IsNullOrEmpty(message) )
-            {
-                MessageBox.Show("Enter message first");
-                return;
-            }
-
-            using (UdpClient udpClient = new UdpClient())
-            {
-                try
-                {
-                    byte[] buff = Encoding.UTF8.GetBytes(message);
-                    IPAddress address = IPAddress.Parse(textBox1.Text);
-                    int port = int.Parse(textBox2.Text);
-                    udpClient.Send(buff, buff.Length, new IPEndPoint(address, port));
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-            }
-        }
+      
     }
 }
