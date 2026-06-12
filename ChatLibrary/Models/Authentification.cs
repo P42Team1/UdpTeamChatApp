cf79a7405a7e12bafb46a951b334c623c122a198
1odc <vlgamerm12349@gmail.com>
2026-06-12 21:02:20 +0200

    Models folder added

--- ChatLibrary/Models/Authentification.cs
+++ ChatLibrary/Models/Authentification.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-namespace ChatLibrary
+namespace ChatLibrary.Models
 {
     public class RegisterPayload
     {
--- UdpTeamChatApp/Form1.cs
+++ UdpTeamChatApp/Form1.cs
@@ -1,7 +1,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using ChatLibrary;
+using ChatLibrary.Models;
 using Microsoft.VisualBasic.Logging;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
